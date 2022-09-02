0519b02f282c8f05e39605a6f0ee50444d327ddc
Kristyna Kupkova <kristynakupkova@gmail.com>
2022-09-02 17:02:48 -0400

    update bw generation

--- normalized_coverageFiles.sh
+++ normalized_coverageFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #the script takes the BAM file, normalizes it base on the normalization factor you give it and created bw file
+# get chromosome sizes with refgenie
+chromSize=$(refgenie seek hg19/fasta.chrom_sizes)
 
 #the BAM file to be normalized
 echo -e What is the name of BAM file?
@@ -15,7 +17,7 @@ suffix="_normalized.bg"
 suffix_bw="_normalized.bw"
 
 #get normalized bg file
-bedtools genomecov -ibam $controlSample -bg -scale $X -g ~/gentools/chromsize/hg19chrom.sizes > $id$suffix
+bedtools genomecov -ibam $controlSample -bg -scale $X -g $chromSize > $id$suffix
 
 #transform bg file to bw file
-wigToBigWig -clip $id$suffix ~/gentools/chromsize/hg19chrom.sizes $id$suffix_bw
+wigToBigWig -clip $id$suffix $chromSize $id$suffix_bw
